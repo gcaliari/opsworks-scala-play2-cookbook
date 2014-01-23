f0273cd1286e0cbe517170d4c4357075d45e0310
Giuliano <giuliano@99taxis.com>
2014-01-23 10:52:35 -0200

    removed stage command from setup

--- play2/definitions/opsworks_play2.rb
+++ play2/definitions/opsworks_play2.rb
@@ -114,7 +114,7 @@ define :opsworks_play2 do
         execute "package #{application}" do
           cwd app_dir
           user "root"
-          command "play clean stage"
+          command "play clean"
         end
 
         # Create the service for the application
