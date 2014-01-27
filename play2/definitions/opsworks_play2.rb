ffae5666b771a5a7b42762f1d9a8c0585f59887e
Giuliano <giuliano@99taxis.com>
2014-01-27 20:40:45 -0200

    setting script name on chef json

--- play2/definitions/opsworks_play2.rb
+++ play2/definitions/opsworks_play2.rb
@@ -132,6 +132,7 @@ define :opsworks_play2 do
             :name => application,
             :path => app_dir,
             :deploy_to => deploy[:deploy_to],
+            :stage_script => play_script_name(),
             :options => play_options()
           })
         end
