5a1da91d58a0da8b4619afa58bacfd865ca7c6c9
Giuliano <giuliano@99taxis.com>
2014-01-28 19:49:22 -0200

    testing opsworks naming conventions for scripts

--- play2/definitions/opsworks_play2.rb
+++ play2/definitions/opsworks_play2.rb
@@ -129,7 +129,6 @@ define :opsworks_play2 do
             :name => application,
             :path => app_dir,
             :deploy_to => deploy[:deploy_to],
-            :script_name => play_script_name(),
             :options => play_options()
           })
         end
