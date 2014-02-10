7853022a0c8f6b012c107d7fd5b436824ee963ff
Ricardo Lazaro de Oliveira <lazaro@99taxis.com>
2014-02-10 20:23:16 -0200

    change commands ordering

--- splunkforwarder/recipes/service.rb
+++ splunkforwarder/recipes/service.rb
@@ -7,26 +7,18 @@
 # Author:: Greg Albrecht <mailto:gba@splunk.com>
 # Copyright:: Copyright 2012 Splunk, Inc.
 # License:: Apache License 2.0
-#
-# Recipe:: service
-# Cookbook Name:: splunkstorm
-# Source:: https://github.com/ampledata/cookbook-splunkforwarder
-# Author:: Greg Albrecht <mailto:gba@splunk.com>
-# Copyright:: Copyright 2012 Splunk, Inc.
-# License:: Apache License 2.0
-
 
 execute '/opt/splunkforwarder/bin/splunk enable boot-start --accept-license' +
     ' --answer-yes' do
   not_if{ File.symlink?('/etc/rc4.d/S20splunk') }
 end
 
-execute '/opt/splunkforwarder/bin/splunk install' +
-  ' app /opt/splunkforwarder/stormforwarder_91dd83d0927411e3b36f123139097a14.spl' +
-  ' -auth admin:changeme' do
-end
-
 service 'splunk' do
   action [:start]
   supports :status => true, :start => true, :stop => true, :restart => true
+end
+
+execute '/opt/splunkforwarder/bin/splunk install' +
+  ' app /opt/splunkforwarder/stormforwarder_91dd83d0927411e3b36f123139097a14.spl' +
+  ' -auth admin:changeme' do
 end
