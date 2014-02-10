cccb866468b04abbdc1220c504dd323b43086b53
Ricardo Lazaro de Oliveira <lazaro@99taxis.com>
2014-02-10 19:22:25 -0200

    echo command

--- splunkforwarder/recipes/service.rb
+++ splunkforwarder/recipes/service.rb
@@ -17,7 +17,9 @@ end
 
 execute '/opt/splunkforwarder/bin/splunk install' +
   'app /opt/splunkforwarder/stormforwarder_91dd83d0927411e3b36f123139097a14.spl' +
-  '-auth admin:changeme'
+  '-auth admin:changeme' do
+  command "echo 'running custom credentials'"
+end
 
 service 'splunk' do
   action [:start]
