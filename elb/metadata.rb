43b2c2aea524e46be891397501578128590ea21d
Ricardo Lazaro de Oliveira <lazaro@99taxis.com>
2015-01-06 19:10:25 -0200

    renamed receipe

--- elb/metadata.rb
+++ elb/metadata.rb
@@ -1,10 +1,6 @@
-name              "opsworks-elb"
+name              "elb"
 maintainer        "CrowdMob Inc."
 maintainer_email  "developers@crowdmob.com"
 license           "Apache 2.0"
 description       "Provides recipes to register and deregister from elb for an opsworks instance"
 version           "0.0.2"
-
-recipe            "opsworks-elb::register"
-recipe            "opsworks-elb::deregister"
-depends           "java"
