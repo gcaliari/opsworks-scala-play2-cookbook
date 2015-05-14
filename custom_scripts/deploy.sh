20d58f85df328ef81983bc301e526ceff1409cff
Giuliano <giuliano@99taxis.com>
2015-05-14 16:06:44 -0300

    Added deploy option for API layer

--- custom_scripts/deploy.sh
+++ custom_scripts/deploy.sh
@@ -13,15 +13,16 @@ done
 echo ''
 echo ''
 echo "=======>  APPLICATION TO DEPLOY:"
-select yn in "PaymentsWeb" "PaymentsBG" "CorporateWeb" "CorporateBG" "PromotionsWeb" "FinanceWeb" "JobsWeb"; do
+select yn in "PaymentsWeb" "PaymentsBG" "CorporateWeb" "CorporateAPI" "CorporateBG" "PromotionsWeb" "FinanceWeb" "JobsWeb"; do
     case $yn in
         PaymentsWeb ) APP='PaymentsWeb';  break;;
         PaymentsBG ) APP='PaymentsBG';  break;;
-		CorporateWeb ) APP='CorporateWeb';  break;;
-		CorporateBG ) APP='CorporateBG';  break;;
-		PromotionsWeb ) APP='PromotionsWeb';  break;;
-		FinanceWeb ) APP='FinanceWeb';  break;;
-		JobsWeb ) APP='JobsWeb';  break;;
+        CorporateWeb ) APP='CorporateWeb';  break;;
+        CorporateAPI ) APP='CorporateAPI';  break;;
+    		CorporateBG ) APP='CorporateBG';  break;;
+        PromotionsWeb ) APP='PromotionsWeb';  break;;
+        FinanceWeb ) APP='FinanceWeb';  break;;
+        JobsWeb ) APP='JobsWeb';  break;;
     esac
 done
 
@@ -33,6 +34,8 @@ case $DEPLOY_ENV in
 		case $APP in
 		    PaymentsWeb ) APPLICATION='server_payments'; STACK_NAME='paymentsstaging'; LAYER_NAME='play2'; break;;
 			CorporateWeb ) APPLICATION='server_corporate'; STACK_NAME="corporatestaging"; LAYER_NAME='play2'; break;;
+      CorporateAPI ) APPLICATION='server_corporate'; STACK_NAME='corporatestaging'; LAYER_NAME='play2-api'; break;;
+			CorporateBG ) APPLICATION='server_corporate'; STACK_NAME='corporatestaging'; LAYER_NAME='play2-background'; break;;
 			PromotionsWeb ) APPLICATION='server_promotions'; STACK_NAME="promotionsstaging"; LAYER_NAME='play2'; break;;
 			FinanceWeb ) APPLICATION='server_finance'; STACK_NAME="financestaging"; LAYER_NAME='play2'; break;;
 			JobsWeb ) APPLICATION='server_taxijobs'; STACK_NAME="taxijobsstaging"; LAYER_NAME='play2';
@@ -43,7 +46,8 @@ case $DEPLOY_ENV in
 		    PaymentsWeb ) APPLICATION='server_payments'; STACK_NAME='payments'; LAYER_NAME='play2'; break;;
 			PaymentsBG ) APPLICATION='server_payments'; STACK_NAME='payments'; LAYER_NAME='play2-background'; break;;
 			CorporateWeb ) APPLICATION='server_corporate'; STACK_NAME='corporate'; LAYER_NAME='play2'; break;;
-			CorporateBG ) APPLICATION='server_corporate'; STACK_NAME='corporate'; LAYER_NAME='play2-background'; break;; 
+      CorporateAPI ) APPLICATION='server_corporate'; STACK_NAME='corporate'; LAYER_NAME='play2-api'; break;;
+			CorporateBG ) APPLICATION='server_corporate'; STACK_NAME='corporate'; LAYER_NAME='play2-background'; break;;
 			PromotionsWeb ) APPLICATION='server_promotions'; STACK_NAME='promotions'; LAYER_NAME='play2'; break;;
 			FinanceWeb ) APPLICATION='server_finance'; STACK_NAME="finance"; LAYER_NAME='play2'; break;;
 			JobsWeb ) APPLICATION='server_taxijobs'; STACK_NAME="taxijobs"; LAYER_NAME='play2';
