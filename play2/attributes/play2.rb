a71044c871bf6a2c796b5b29d07ed0ec276d42af
Miguel Mendes Ruiz <migmruiz@gmail.com>
2015-07-13 17:02:49 -0300

    changing defaults to activator 1.3.5

--- play2/attributes/play2.rb
+++ play2/attributes/play2.rb
@@ -1,6 +1,6 @@
 default[:play2][:version]           = "2.2.1"
 default[:play2][:url]               = "http://downloads.typesafe.com/play"
-default[:play2][:activator_version] = "1.2.2"
+default[:play2][:activator_version] = "1.3.5"
 default[:play2][:activator_url] = "http://downloads.typesafe.com/typesafe-activator"
 
 default[:play2][:conf]     = nil
