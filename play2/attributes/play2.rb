009415979a8dd7a08a4a82f91b8657c11c6f318c
Giuliano Caliari <giuliano@99taxis.com>
2015-07-31 10:16:56 -0300

    update play version

--- play2/attributes/play2.rb
+++ play2/attributes/play2.rb
@@ -1,4 +1,4 @@
-default[:play2][:version]           = "2.2.1"
+default[:play2][:version]           = "2.4.2"
 default[:play2][:url]               = "http://downloads.typesafe.com/play"
 default[:play2][:activator_version] = "1.3.5"
 default[:play2][:activator_url] = "http://downloads.typesafe.com/typesafe-activator"
